eb679b489d2b76bd63cc8a9be4eb421ba61094e8
Dylan Ochoa <Dylan.Ochoa@GMR.local>
2019-01-06 16:34:32 -0800

    Finsihed the displaying in order to broaden my understanding of APIs. Created the definition getter. Decided to exclude the short definitions as they were 5 words off at most. Next time use the test in order to get the part of speach

--- DictionaryAPITest/Program.cs
+++ DictionaryAPITest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -9,55 +10,120 @@ namespace DictionaryAPITest
     {
         static async Task Main(string[] args)
         {
+            //try
+            //{
             var client = new HttpClient();
             client.DefaultRequestHeaders.Add("app_id", "e8e7ecee");
             client.DefaultRequestHeaders.Add("app_key", "610cfa0b0947d48788edd521c5542dfb");
 
-            string word = "ring";
+            string word = "king";
 
             string pull = await client.GetStringAsync($"https://od-api.oxforddictionaries.com:443/api/v1/entries/en/{word.ToLower()}").ConfigureAwait(false);
             var definitionObject = JsonConvert.DeserializeObject<WordDef>(pull);
-
-            foreach (var result in definitionObject.Results)
+            List<string> definitions = new List<string>();
+            if (definitionObject.Results != null)
             {
-                Console.WriteLine($"Word: {result.word}");
-                foreach (var lexicalEntry in result.lexicalEntries)
+                foreach (var result in definitionObject.Results)
                 {
-                    Console.WriteLine($"\tLexical Category: {lexicalEntry.lexicalCategory}");
-                    foreach (var entry in lexicalEntry.entries)
+                    if (result.lexicalEntries != null)
                     {
-                        Console.WriteLine("\t\tEtymologies:");
-                        foreach (var etymology in entry.etymologies)
+                        foreach (var lexicalEntry in result.lexicalEntries)
                         {
-                            Console.WriteLine($"\t\t - {etymology}");
-                        }
-
-                        Console.WriteLine();
-
-                        Console.WriteLine("\t\tSenses:");
-                        foreach (var sense in entry.senses)
-                        {
-                            Console.WriteLine($"\t\t - {sense.definitions[0]}");
-                            Console.WriteLine($"\t\t\tShort Definition: {sense.short_definitions[0]}");
-                            Console.WriteLine();
-                            Console.WriteLine("\t\t\tSubsenses");
-                            if (sense.subsenses == null)
+                            if (lexicalEntry.entries != null)
                             {
-                                continue;
-                            }
-                            foreach(var subsense in sense.subsenses)
-                            {
-                                if (subsense.definitions == null || subsense.short_definitions == null)
+                                foreach (var entry in lexicalEntry.entries)
                                 {
-                                    continue;
+                                    if (entry.senses != null)
+                                    {
+                                        foreach (var sense in entry.senses)
+                                        {                                            
+                                            if (sense.definitions != null && sense.definitions.Length > 0 && sense.definitions[0] != null)
+                                            {
+                                                definitions.Add(sense.definitions[0]);
+                                                Console.WriteLine(sense.definitions[0]);
+                                                Console.WriteLine("");
+                                            }
+                                            if (sense.subsenses != null)
+                                            {
+                                                foreach (var subsense in sense.subsenses)
+                                                {
+                                                    if (subsense.definitions != null && subsense.definitions.Length > 0 && subsense.definitions[0] != null)
+                                                    {
+                                                        definitions.Add(subsense.definitions[0]);
+                                                        Console.WriteLine(subsense.definitions[0]);
+                                                        Console.WriteLine("");
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
                                 }
-                                Console.WriteLine($"\t\t\t - {subsense.definitions[0]}");
-                                Console.WriteLine($"\t\t\t\tShort Definition: {subsense.short_definitions[0]}");
                             }
                         }
                     }
                 }
             }
+            //foreach (var result in definitionObject.Results)
+            //{
+            //    Console.WriteLine($"Word: {result.word}");
+            //    foreach (var lexicalEntry in result.lexicalEntries)
+            //    {
+            //        Console.WriteLine($"\tLexical Category: {lexicalEntry.lexicalCategory}");
+            //        
+            //        {
+            //            Console.WriteLine("\t\tEtymologies:");
+            //            if (entry.etymologies != null)
+            //            {
+            //                foreach (var etymology in entry.etymologies)
+            //                {
+            //                    Console.WriteLine($"\t\t - {etymology}");
+            //                }
+            //            }
+
+            //            Console.WriteLine();
+            //            Console.WriteLine("\t\tSenses:");
+            //            if (entry.senses == null)
+            //            {
+            //                continue;
+            //            }
+            //            foreach (var sense in entry.senses)
+            //            {
+            //                if (sense.definitions[0] != null)
+            //                {
+            //                    Console.WriteLine($"\t\t - {sense.definitions[0]}");
+            //                }
+            //                if (sense.short_definitions[0] != null)
+            //                {
+            //                    Console.WriteLine($"\t\t\tShort Definition: {sense.short_definitions[0]}");
+            //                }
+
+            //                Console.WriteLine();
+            //                Console.WriteLine("\t\t\tSubsenses");
+            //                if (sense.subsenses == null)
+            //                {
+            //                    continue;
+            //                }
+            //                foreach (var subsense in sense.subsenses)
+            //                {
+            //                    if (subsense.definitions[0] != null)
+            //                    {
+            //                        Console.WriteLine($"\t\t\t - {subsense.definitions[0]}");
+            //                    }
+            //                    if (subsense.short_definitions[0] != null)
+            //                    {
+            //                        Console.WriteLine($"\t\t\t\tShort Definition: {subsense.short_definitions[0]}");
+            //                    }
+            //                }
+            //            }
+
+            //        }
+            //    }
+            //}
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex);
+            //}
 
             //var x = definitionObject.Results.FirstOrDefault()?.lexicalEntries.FirstOrDefault()?.entries.FirstOrDefault()?.senses.FirstOrDefault();
             //var definitions = x.definitions != null && x.definitions.Length > 0 ? x.definitions : x.subsenses.FirstOrDefault()?.definitions;
